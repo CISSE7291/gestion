4b120152528b121c16fa08b312a61ee86cca15a6
CISSE7291 <a72911488@gmail.com>
2023-06-16 11:08:15 +0200

    startcisse

---
+++ listeeleve.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+  <table class="table table-fixed">
+    <thead>
+      <tr>
+    <th class="col-xs-3">N</th>
+        <th class="col-xs-3">Last Name</th>
+        <th class="col-xs-6">E-mail</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+      <tr>
+        <td class="col-xs-3">John</td>
+        <td class="col-xs-3">Doe</td>
+        <td class="col-xs-6">johndoe@email.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
